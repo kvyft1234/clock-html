224111f760ac503d52cc8c4942232d0b964809cc
kvyft1234 <48409066+kvyft1234@users.noreply.github.com>
2021-07-13 23:32:06 +0900

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,21 @@
+var clock1 = document.querySelectorAll('p')[0];
+var clock2 = document.querySelectorAll('pre')[0];
+var year, month, day, hour, minute, second;
+
+function modify(a,n){
+	let k = String(a);
+	for(let i=k.length; i<n; i++){
+		k = "0"+k;
+	}
+	return k;
+}
+
+function update(){
+	let d = new Date();
+	[year, month, day] = [d.getFullYear(), d.getMonth(), d.getDay()];
+	[hour, minute, second] = [d.getHours(), d.getMinutes(), d.getSeconds()];
+	clock1.innerText = `${modify(year,4)}년 ${modify(month,2)}월 ${modify(day,2)}일`
+	clock2.innerText = `${modify(hour,2)}시 ${modify(minute,2)}분 ${modify(second,2)}초`
+}
+
+setInterval(update,1000);
